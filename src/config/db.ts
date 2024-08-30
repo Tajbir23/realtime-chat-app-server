abd240f0163ff27fc50f6bf7e65f7c7019fc5aba
Tajbir23 <studenttajbirislam@gmail.com>
2024-08-30 17:49:22 +0600

    test

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose"
 
 const connection = async() => {
     try {
-        await mongoose.connect(process.env.mongodb_uri || 'mongodb://localhost:27017')
+        // await mongoose.connect(process.env.mongodb_uri || 'mongodb://localhost:27017')
+        await mongoose.connect('mongodb+srv://realtimeChat:realtimeChat@cluster0.sdyx3bs.mongodb.net/realtimeChat?retryWrites=true&w=majority&appName=Cluster0')
         console.log('Connected to MongoDB')
     } catch (err) {
         console.log('mongodb error', err)
