521b3cc193318c117c6ed385f03182d04610b3aa
Tajbir23 <studenttajbirislam@gmail.com>
2024-09-04 13:54:51 +0600

    test

--- src/index.ts
+++ src/index.ts
@@ -17,12 +17,12 @@ const server = createServer(app)
 
 export const io = new Server(server, {
     cors: {
-        origin: '*',
+        origin: ['http://localhost:5173'],
     }
 })
 
 app.use(cors({
-    origin: '*',
+    origin: ['http://localhost:5173'],
 }))
 
 app.use(express.json())
@@ -71,6 +71,7 @@ io.on('connection', (socket) => {
 
 })
 
+// export default server
 server.listen(port, async() => {
     console.log('Server is running on port 3000')
 })
