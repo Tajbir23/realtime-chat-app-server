65b6cda6c9668fa82950d3d1f94bb1938669e3b7
Tajbir23 <studenttajbirislam@gmail.com>
2024-10-17 09:26:48 +0600

    test

--- dist/config/redis.js
+++ dist/config/redis.js
@@ -12,8 +12,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.subClient = exports.pubClient = void 0;
 const redis_1 = require("redis");
 // const REDIS_URL = 'redis://:5m1aX9UKDIfL9j296DsgIcI8eUAqUObA@redis-10369.c44.us-east-1-2.ec2.redns.redis-cloud.com:10369';
-// const REDIS_URL = 'redis://default:FgxtHeVFUDbKvPacAJvpSpIZUBhsGPop@autorack.proxy.rlwy.net:51415';
-const REDIS_URL = 'redis://127.0.0.1:6379';
+const REDIS_URL = 'redis://default:FgxtHeVFUDbKvPacAJvpSpIZUBhsGPop@autorack.proxy.rlwy.net:51415';
+// const REDIS_URL = 'redis://127.0.0.1:6379';
 exports.pubClient = (0, redis_1.createClient)({
     url: REDIS_URL
 });
