3ca6dcf4406c1193a9c3f354481cabc6c3345d0e
Tajbir23 <studenttajbirislam@gmail.com>
2024-10-31 01:20:35 +0600

    test

--- src/handler/socket/connection/detectMultipleConnection.ts
+++ src/handler/socket/connection/detectMultipleConnection.ts
@@ -3,7 +3,7 @@ import { connectedUsers } from "../../.."
 const detectMultipleConnection = (userId: string) => {
     const userDevices = connectedUsers[userId]
     const deviceCount = Object.values(userDevices).length;
-    if(deviceCount > 0){
+    if(deviceCount > 1){
         console.log(`User ${userId} has multiple connected devices: ${deviceCount}`);
         return true;
     }
