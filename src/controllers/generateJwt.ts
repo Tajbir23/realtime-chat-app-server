e0c75e838ea8a9cf79841ac08b59df501a9ca145
Tajbir23 <studenttajbirislam@gmail.com>
2024-12-30 22:52:13 +0700

    import dotenv

--- src/controllers/generateJwt.ts
+++ src/controllers/generateJwt.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import jwt from 'jsonwebtoken'
 const generateJwt = async (username:string, email:string, _id?:string,) => {
     const token = await jwt.sign({ username, email, _id }, process.env.jwt_secret as string, { expiresIn: '7d' })
