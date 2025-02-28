4caf1a174a4be92b7830b537f827cb90df6fe136
Tajbir23 <studenttajbirislam@gmail.com>
2025-02-28 22:21:36 +0600

    test

--- dist/config/redis.js
+++ dist/config/redis.js
@@ -1,12 +1,15 @@
 "use strict";
+// import Redis from "ioredis";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// const redis = new Redis({
+//     host: process.env.REDIS_HOST,
+//     port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : undefined,
+//     password: process.env.REDIS_PASSWORD,
+// })
+// export default redis;
 const ioredis_1 = __importDefault(require("ioredis"));
-const redis = new ioredis_1.default({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : undefined,
-    password: process.env.REDIS_PASSWORD,
-});
+const redis = new ioredis_1.default(process.env.REDIS_URL);
 exports.default = redis;
--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,9 +1,15 @@
+// import Redis from "ioredis";
+
+// const redis = new Redis({
+//     host: process.env.REDIS_HOST,
+//     port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : undefined,
+//     password: process.env.REDIS_PASSWORD,
+// })
+
+// export default redis;
+
 import Redis from "ioredis";
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : undefined,
-    password: process.env.REDIS_PASSWORD,
-})
+const redis = new Redis(process.env.REDIS_URL as string);
 
 export default redis;
